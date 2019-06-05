9e36db28b5c5d293cbfe09e2132212ebde79a0cc
KRIS DAVIS <krissdavis18@mail.com>
2019-06-05 07:37:52 +0000

    Done.

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -2,7 +2,7 @@ require "pry"
 require './lib/intro_to_simple_array_manipulations.rb'
 
 
-describe "#using_push" do 
+describe .push do 
   it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
     countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     next_country = "Niger"
@@ -11,14 +11,14 @@ describe "#using_push" do
     end
 end
 
-describe "#using_unshift" do 
+describe .unshift do 
   before(:each) do 
     neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
     @new_neighborhood = "Brooklyn Heights"
     @updated_array = using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
   end
   
-  it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
+  it .first do
     expect(@updated_array.first).to eq(@new_neighborhood)
   end
 
@@ -27,12 +27,12 @@ describe "#using_unshift" do
   end
 end
 
-describe "#using_pop" do
+describe .pop do
   before(:each) do 
     @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
     @deleted_string = using_pop(@great_hits_of_the_nineties)
   end
-  it "takes in argument of an array and uses the pop method to remove the last element from the array and return that element" do 
+  it  .last  do 
     expect(@deleted_string).to eq("Losing My Religion")
   end
 
